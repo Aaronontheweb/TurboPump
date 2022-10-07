d314cf56073fac2b7b3f6c49508ffd9236769062
Aaron Stannard <aaron@petabridge.com>
2022-10-07 07:07:40 -0500

    initial re-structuring of solution (#3)

---
+++ src/TurboPump/WorkQueue.cs
@@ -0,0 +1,17 @@
+﻿namespace TurboPump
+{
+    /// <summary>
+    /// An asynchronous operation will be executed by a thread.
+    /// </summary>
+    public interface IRunnable
+    {
+        /// <summary>
+        /// Executes the action.
+        /// </summary>
+        void Run();
+    }
+    
+    public class WorkQueue
+    {
+    }
+}
